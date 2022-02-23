77b7af587ab37d1bb8d339a42deb89c6811c5e9a
Waner <waner.miranda@picpay.com>
2022-02-23 14:20:20 -0300

    Added an table generator to the base controller

--- apps/home/tests.py
+++ apps/home/tests.py
@@ -21,4 +21,36 @@ class BaseControllerTests(TestCase):
 
     def test_generate_table(self):
         controller = BaseController(session=self._session, document_adapter=self._document_adapter, configuration=self._configuration, data_klass=Feature)
-        assert not controller
+        assert controller
+        data = [{
+                "origin": "fg_client_unit_group_test",
+                "name": "day",
+                "updated_at": "1645631141",
+                "created_at": "1645631145",
+                "dag": "",
+                "source": "",
+                "type": "int",
+                "desc": "particao: dia"
+                },
+                {
+                "origin": "fg_client_unit_group_test",
+                "name": "day",
+                "updated_at": "1645631141",
+                "created_at": "1645631145",
+                "dag": "",
+                "source": "",
+                "type": "int",
+                "desc": "particao: dia"
+                },{
+                "origin": "fg_client_unit_group_test",
+                "name": "day",
+                "updated_at": "1645631141",
+                "created_at": "1645631145",
+                "dag": "",
+                "source": "",
+                "type": "int",
+                "desc": "particao: dia"
+                }]
+        rendered_table = controller.render_table(data) 
+        assert len(rendered_table.rows) == 3 
+        assert len(rendered_table.base_columns) == 8
