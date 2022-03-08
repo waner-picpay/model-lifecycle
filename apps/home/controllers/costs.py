1361a74b691d46276b4fc2b7725f0888d8dbfb76
Waner <waner.miranda@picpay.com>
2022-03-08 12:44:39 -0300

    Split the requirements between deploy and dev

--- apps/home/controllers/costs.py
+++ apps/home/controllers/costs.py
@@ -1,43 +1,49 @@
 import boto3
 
-client = boto3.client('ce')
+from copy import deepcopy
+from typing import Dict, List, Type
+from boto3 import Session
+from apps.home.controllers.base import BaseController
+from apps.home.entities.metric import Metric
+from apps.home.factories.logger import LoggerFactory
+from apps.home.store.interfaces import IDocumentAdapter
+from apps.home.utils.configuration import Configuration
+from urllib.parse import quote_plus as urlencode
+logger = LoggerFactory.get_logger(__name__)
+class CostsExplorer(BaseController):
+    def __init__(self, session: Session = None, configuration: Configuration = None, document_adapter: IDocumentAdapter = None, data_klass: Type = None):
+        super().__init__(session, configuration, document_adapter, data_klass)
+        self._client = boto3.client('ce')
 
-response = client.get_cost_and_usage(
-    TimePeriod={
-        'Start': '2022-01-01',
-        'End': '2022-01-31'
-    },
-    Granularity='MONTHLY',
-    # Filter={
-    #     'Or': [
-    #         {'... recursive ...'},
-    #     ],
-    #     'And': [
-    #         {'... recursive ...'},
-    #     ],
-    #     'Not': {'... recursive ...'},
-    #     'Dimensions': {
-    #         'Key': 'AZ'|'INSTANCE_TYPE'|'LINKED_ACCOUNT'|'OPERATION'|'PURCHASE_TYPE'|'REGION'|'SERVICE'|'USAGE_TYPE'|'USAGE_TYPE_GROUP'|'RECORD_TYPE'|'OPERATING_SYSTEM'|'TENANCY'|'SCOPE'|'PLATFORM'|'SUBSCRIPTION_ID'|'LEGAL_ENTITY_NAME'|'DEPLOYMENT_OPTION'|'DATABASE_ENGINE'|'CACHE_ENGINE'|'INSTANCE_TYPE_FAMILY'|'BILLING_ENTITY'|'RESERVATION_ID',
-    #         'Values': [
-    #             'string',
-    #         ]
-    #     },
-    #     'Tags': {
-    #         'Key': 'string',
-    #         'Values': [
-    #             'string',
-    #         ]
-    #     }
-    # },
-    Metrics=[
-        'BLENDED_COST',
-    ],
-    GroupBy=[
-        {
-            'Type': 'TAG',
-            'Key': 'ProcessType'
-        },
-    ],
-    # NextPageToken='string'
-)
-print(response)
+    def get_costs_usage(self, start_date:str, end_date:str):
+        response = self._client.get_cost_and_usage(
+            TimePeriod={
+                'Start': start_date,
+                'End': end_date
+            },
+            Granularity='MONTHLY',
+            # Filter={
+                
+            #     'Tags': {
+            #         'Key': 'ProcessType',
+            #         'Values': [
+            #             'string',
+            #         ]
+            #     }
+            # },
+            Metrics=[
+                'BLENDED_COST',
+            ],
+            GroupBy=[
+                {
+                    'Type': 'TAG',
+                    'Key': 'ProcessType'
+                },
+            ]
+        )
+        return response
+
+
+ce = CostsExplorer()
+
+print(ce.get_costs_usage())
